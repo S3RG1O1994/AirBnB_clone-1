8f61cf5453e42df33ddf867b5565bd99e8fc457e
S3RG1O1994 <se.diaz60@gmail.com>
2020-08-13 15:37:14 +0000

    [Update] models place, amenity and state, [Add]: __init__ for import all class or models

--- models/place.py
+++ models/place.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+from os import environ
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
+import models
+
+place_amenity = Table(
+    'place_amenity', 
+    Base.metadata, 
+    Column('place_id', String(60), ForeignKey('places.id'), nullable=False, primary_key=True), 
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), nullable=False, primary_key=True)
+)
 
 
 class Place(BaseModel, Base):
@@ -19,14 +28,31 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float)
     longitude = Column(Float)
-    reviews = relationship('Review', cascade="all, delete, delete-orphan", backref='place')
     amenity_ids = []
 
-    @property
-    def reviews(self):
-        ret_list = []
-        review_dir = models.storage.all(Review)
-        for sub_dir in review_dir.values():
-            if sub_dir.place_id is self.id:
-                ret_list.append(sub_dir)
-        return ret_list
+    if environ.get('HBNB_TYPE_STORAGE') == 'db':
+        reviews = relationship('Review', cascade="all, delete, delete-orphan", backref='place')
+        amenities = relationship('Amenity', secondary='place_amenity', viewonly=False)
+    else:
+        @property
+        def reviews(self):
+            ret_list = []
+            review_dir = models.storage.all(Review)
+            for sub_dir in review_dir.values():
+                if sub_dir.place_id is self.id:
+                    ret_list.append(sub_dir)
+            return ret_list
+        
+        @property
+        def amenities(self):
+            ret_list = []
+            amenitie_dir = models.storage.all(Amenity)
+            for sub_dir in amenitie_dir.values():
+                if sub_dir.place_id is self.id:
+                    ret_list.append(sub_dir)
+            return ret_list
+
+        @amenities.setter
+        def amenities(self, Amenity_obj=None):
+            if isinstance(Amenity_obj, models.Amenity):
+                self.amenity_ids.append(Amenity_obj.id)
--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from os import environ
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -10,8 +11,10 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade="all, delete, delete-orphan", backref='state')
 
+    #if environ.get('HBNB_TYPE_STORAGE') == 'db':
+    cities = relationship('City', cascade="all, delete, delete-orphan", backref='state')
+    #else:
     @property
     def cities(self):
         ret_list = []
