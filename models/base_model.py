b83a2a59b5fa550f6f55a33bf973be64fcd822dc
Cris-Mur <yomy98@gmail.com>
2020-08-13 20:15:16 +0000

    [fix]base_model forgot save in data_base

--- models/base_model.py
+++ models/base_model.py
@@ -33,7 +33,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            models.storage.new(self)
+        self.save()
 
     def __str__(self):
         """Returns a string representation of the instance"""
