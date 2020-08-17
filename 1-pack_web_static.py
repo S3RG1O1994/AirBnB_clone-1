19f14781950b7fa2c9202d15598f15b97d0533b2
S3RG1O1994 <se.diaz60@gmail.com>
2020-08-17 17:16:26 -0500

    [Fix]: pep8 code styles

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,14 +1,15 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+
 from fabric.api import local
 import datetime
 
 
 def do_pack():
     '''Function for generate pack all files'''
-    
     actual = datetime.datetime.now()
     local('mkdir -p versions/')
-    pack = 'versions/web_static_{}{}{}{}{}.tgz'.format(actual.year, actual.month,
+    pack = 'versions/web_static_{}{}{}{}{}.tgz'.format(
+                    actual.year, actual.month,
                     actual.day, actual.minute, actual.second)
     local('sudo tar -cvzf {} web_static'.format(pack))
     if pack:
