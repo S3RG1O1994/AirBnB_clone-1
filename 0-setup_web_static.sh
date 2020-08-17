d7d6f0a5b1a8e2cf67b67e9c6ea4a4883e5f0887
S3RG1O1994 <se.diaz60@gmail.com>
2020-08-17 18:42:01 -0500

    [fix]: Corrections of execution -sudo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,8 +3,8 @@
 sudo apt-get -y upgrade
 sudo apt-get -y update
 sudo apt-get -y install nginx
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared
 echo "<html>
     <head>
     </head>
@@ -12,7 +12,7 @@ echo "<html>
         Holberton School
     </body>
 </html>" >> /data/web_static/releases/test/index.html
-ln -s /data/web_static/releases/test /data/web_static/current
+ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
-sudo service nginx start
+sudo service nginx restart
