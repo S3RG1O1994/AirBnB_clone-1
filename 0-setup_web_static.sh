253e62c904723febd01c20b6ade34ecddff0cff5
S3RG1O1994 <se.diaz60@gmail.com>
2020-08-17 18:45:36 -0500

    [fix]: Corrections of execution -sudo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,7 +10,7 @@ echo "<html>
     <body>
         Holberton School
     </body>
-</html>" >> /data/web_static/releases/test/index.html
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
