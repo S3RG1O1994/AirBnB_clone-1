39978eb7197f96d22d72d7310ecbe33c7f793f10
S3RG1O1994 <se.diaz60@gmail.com>
2020-09-01 22:58:31 -0500

    [Fix]: identation using tabs

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 
 @app.route('/hbnb', strict_slashes=False)
 def index_hbtn():
-    '''This return string in the page'''
-    return 'HBNB'
+	'''This return string in the page'''
+	return 'HBNB'
 
 
 @app.route('/', strict_slashes=False)
 def index():
-    '''This return string in the page'''
-    return 'Hello HBNB!'
+	'''This return string in the page'''
+	return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+	app.run(host='0.0.0.0', port=5000)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -7,27 +7,26 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    '''This return string in the page'''
-    return 'HBNB'
+	'''This return string in the page'''
+	return 'HBNB'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def index_hbnb():
-    '''This return string in the page'''
-    return 'Hello HBNB!'
+	'''This return string in the page'''
+	return 'Hello HBNB!'
 
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes=False)
 def index_c(text):
-    '''This is return string pass in text'''
-    return 'C {}'.format(text)
+	'''This is return string pass in text'''
+	return 'C {}'.format(text)
 
 
-@app.route('/python')
-@app.route('/python/')
-@app.route('/python/<string:text>')
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
 def index_python(text='is cool'):
-    return 'python {}'.format(text)
+	return 'python {}'.format(text)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+	app.run(host='0.0.0.0', port=5000)
