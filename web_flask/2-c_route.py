2f789e0a67396f9419d0846c1764994d2deb6f23
S3RG1O1994 <se.diaz60@gmail.com>
2020-09-01 23:49:05 -0500

    [Fix]: mode comment

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-from flask import Flask
 '''This function is for edit html'''
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def index():
-	'''This return string in the page'''
-	return 'HBNB'
+    '''This return string in the page'''
+    return 'HBNB'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def index_hbnb():
-	'''This return string in the page'''
-	return 'Hello HBNB!'
+    '''This return string in the page'''
+    return 'Hello HBNB!'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def index_c(text):
-	'''This is return string pass in text'''
-	return 'C {}'.format(text)
+    '''This is return string pass in text'''
+    return 'C {}'.format(text)
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
