18356a78e56650fec55c5832e411d5cbc56bc095
S3RG1O1994 <se.diaz60@gmail.com>
2020-09-02 12:04:36 -0500

    [Add]: New route for print n if n is an number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+'''This function is for edit html'''
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    '''This return string in the page'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def index_hbnb():
+    '''This return string in the page'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def index_c(text):
+    '''This is return string pass in text'''
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def index_python(text='is cool'):
+    '''Python (text)'''
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
